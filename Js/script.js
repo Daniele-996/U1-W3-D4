5065e3325d486b1015127bb026a74190b83b9481
Daniele Sanzari <danielesanza@gamil.com>
2025-03-27 18:03:39 +0100

    commit

---
+++ Js/script.js
@@ -0,0 +1,21 @@
+const createTab = [];
+const main = document.querySelector("main");
+for (let i = 0; i < 90; i++) {
+  createTab.push([]);
+  //console.log(createTab);
+
+  const createDivTab = document.createElement("div");
+  createDivTab.classList.add("selectDiv");
+
+  const createH3DivTab = document.createElement("h3");
+  createH3DivTab.innerText = i + 1;
+
+  main.appendChild(createDivTab);
+  createDivTab.appendChild(createH3DivTab);
+}
+
+const buttonClick = document.querySelector("button");
+buttonClick.onclick = function (event) {
+  const randomNumb = Math.floor(Math.random() * 90) + 1;
+  const divSelect = document.querySelectorAll("div");
+};
