eacf7c31db7690ac18cdf6a7cca4ff178903085e
Daniele Sanzari <danielesanza@gamil.com>
2025-03-27 20:06:48 +0100

    new

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,6 @@
 const createTab = [];
 const main = document.querySelector("main");
+
 for (let i = 0; i < 90; i++) {
   createTab.push([]);
   //console.log(createTab);
@@ -19,3 +20,17 @@ buttonClick.onclick = function (event) {
   const randomNumb = Math.floor(Math.random() * 90) + 1;
   const divSelect = document.querySelectorAll("div");
 };
+
+const createCard = [];
+const section = document.querySelector("section");
+
+for (let i = 0; i < 24; i++) {
+  const createSecCart = document.createElement("div");
+  createSecCart.classList.add("selectCart");
+
+  const createH3DivTab = document.createElement("h3");
+  createH3DivTab.innerText = i + 1;
+
+  main.appendChild(createDivTab);
+  createDivTab.appendChild(createH3DivTab);
+}
